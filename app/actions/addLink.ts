59324e22ac780247a71d6e0f30945b09f9c45e70
paul-chukwuemeka <paulchukwuemeka123@gmail.com>
2026-01-01 21:59:13 +0100

    refactor: uncomment and restore addLink function implementation

--- app/actions/addLink.ts
+++ app/actions/addLink.ts
@@ -1,14 +1,19 @@
-// "use server";
-// import { prisma } from "@/lib/prisma";
-// import { LinkType } from "@/types/types";
+"use server";
+import { prisma } from "@/lib/prisma";
+import { LinkType } from "@/types/types";
 
-// // export async function addLink(
-// //   id: string,
-// //   link: LinkType,
-// //   collectionId?: string
-// // ) {
-// //   const res = await prisma.link.create({
-// //     data: link
-// //   })
-// //   return res;
-// // }
+export async function addLink(
+  id: string,
+  link: LinkType,
+  collectionId?: string
+) {
+  const res = await prisma.link.create({
+    data: {
+      ...link,
+      collection: {
+        connect: { id: collectionId }
+      }
+    }
+  })
+  return res;
+}
